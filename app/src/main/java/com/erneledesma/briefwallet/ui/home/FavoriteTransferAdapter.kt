db333c2a703509d292d2bfeaa5d7279825d9558f
Ernesto Ledesma <ernestoledesma@MacBook-Pro-de-Ernesto.local>
2020-09-26 15:45:36 -0300

    first commit

---
+++ app/src/main/java/com/erneledesma/briefwallet/ui/home/FavoriteTransferAdapter.kt
@@ -0,0 +1,33 @@
+package com.erneledesma.briefwallet.ui.home
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.erneledesma.briefwallet.R
+
+
+class FavoriteTransferAdapter : RecyclerView.Adapter<FavoriteTransferViewHolder>() {
+
+    private lateinit var favoriteTransferItems: List<FavoriteTransfer>
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FavoriteTransferViewHolder =
+            FavoriteTransferViewHolder(
+                    LayoutInflater.from(parent.context).inflate(
+                            R.layout.favorite_transfer_row,
+                            parent,
+                            false
+                    )
+            )
+
+
+    override fun getItemCount(): Int = favoriteTransferItems.size
+
+    override fun onBindViewHolder(holder: FavoriteTransferViewHolder, position: Int) =
+            holder.bind(favoriteTransferItems[position])
+
+    fun setData(favoriteTransferItems: List<FavoriteTransfer>) {
+        this.favoriteTransferItems = favoriteTransferItems
+        notifyDataSetChanged()
+    }
+
+
+}
---
+++ app/src/main/java/com/erneledesma/briefwallet/ui/transfer/OnItemSelected.kt
@@ -0,0 +1,5 @@
+package com.cristianvillamil.briefwallet.ui.transfer
+
+interface OnItemSelected<T> {
+    fun onItemSelected(item: T)
+}
